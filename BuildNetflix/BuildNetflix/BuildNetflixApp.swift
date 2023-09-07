55dcb763b9527681c8944c6d40cacebf7fcd71e5
Henrykim1111 <henrykim9319@gmail.com>
2023-09-07 22:51:16 +0900

    [Section1] git init

---
+++ BuildNetflix/BuildNetflix/BuildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp.swift
+//  BuildNetflix
+//
+//  Created by Hyungmin Kim on 2023/09/07.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
