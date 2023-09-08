cb99e87b87ebd4cb27423349330bc8cb13fa3690
Henrykim1111 <henrykim9319@gmail.com>
2023-09-09 00:44:44 +0900

    [Section2] lecture 3 finished

---
+++ BuildNetflix/BuildNetflix/iOS/HomeVM.swift
@@ -0,0 +1,35 @@
+//
+//  HomeVM.swift
+//  BuildNetflix
+//
+//  Created by Hyungmin Kim on 2023/09/08.
+//
+
+import Foundation
+
+class HomeVM: ObservableObject {
+    
+    // String == Category
+    @Published var movies: [String: [Movie]] = [:]
+    
+    public var allCategories: [String] {
+        movies.keys.map({ String($0) })
+    }
+    
+    public func getMovie(forCat cat: String) -> [Movie] {
+        return movies[cat] ?? []
+    }
+    
+    init() {
+        setupMovies()
+    }
+    
+    func setupMovies() {
+        movies["Trending Now"] = exampleMovies
+        movies["Stand-up Comedy"] = exampleMovies.shuffled()
+        movies["New Release"] = exampleMovies.shuffled()
+        movies["Watch It Again"] = exampleMovies.shuffled()
+        movies["Sci-Fi Movies"] = exampleMovies.shuffled()
+    }
+    
+}
---
+++ BuildNetflix/BuildNetflix/iOS/movie.swift
@@ -0,0 +1,14 @@
+//
+//  movie.swift
+//  BuildNetflix
+//
+//  Created by Hyungmin Kim on 2023/09/07.
+//
+
+import Foundation
+
+struct Movie: Identifiable {
+    var id: String
+    var name: String
+    var thumbnailURL: URL
+}
